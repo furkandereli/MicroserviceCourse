24be89d53bac5f22a2ed187bbc1ae786bbbcce9e
furkandereli <furkandereli98@gmail.com>
2025-11-24 00:05:53 +0300

    Delete file api

---
+++ MicroserviceCourse.File.Api/Features/File/Delete/DeleteFileCommandEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using MicroserviceCourse.Shared.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceCourse.File.Api.Features.File.Delete;
+
+public static class DeleteFileCommandEndpoint
+{
+    public static RouteGroupBuilder DeleteFileGroupItemEndpoint(this RouteGroupBuilder group)
+    {
+        group.MapDelete("/", async ([FromBody] DeleteFileCommand command, IMediator mediator) => (await mediator.Send(command)).ToGenericResult())
+            .WithName("delete")
+            .MapToApiVersion(1, 0)
+            .Produces(StatusCodes.Status204NoContent)
+            .Produces<ProblemDetails>(StatusCodes.Status400BadRequest)
+            .Produces<ProblemDetails>(StatusCodes.Status500InternalServerError);        
+
+        return group;
+    }
+}
--- MicroserviceCourse.File.Api/Features/File/FileEndpointExt.cs
+++ MicroserviceCourse.File.Api/Features/File/FileEndpointExt.cs
@@ -1,4 +1,5 @@
 ﻿using Asp.Versioning.Builder;
+using MicroserviceCourse.File.Api.Features.File.Delete;
 using MicroserviceCourse.File.Api.Features.File.Upload;
 
 namespace MicroserviceCourse.File.Api.Features.File;
@@ -7,9 +8,10 @@ public static class FileEndpointExt
 {
     public static void AddFileGroupEndpointExt(this WebApplication app, ApiVersionSet apiVersionSet)
     {
-        app.MapGroup("/api/v1{version:apiVersion}/files")
+        app.MapGroup("/api/v{version:apiVersion}/files")
             .WithTags("files")
             .WithApiVersionSet(apiVersionSet)
-            .UploadFileGroupItemEndpoint();
+            .UploadFileGroupItemEndpoint()
+            .DeleteFileGroupItemEndpoint();
     }
 }
