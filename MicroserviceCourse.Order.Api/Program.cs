99514bc794efb1938d56eef936814d146b04b0b8
furkandereli <furkandereli98@gmail.com>
2025-11-25 01:40:45 +0300

    Order microservice with clean architecture

---
+++ MicroserviceCourse.Order.Api/Program.cs
@@ -0,0 +1,21 @@
+using MicroserviceCourse.Order.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.Run();
---
+++ MicroserviceCourse.Order.Domain/Entities/OrderItem.cs
@@ -0,0 +1,44 @@
+﻿namespace MicroserviceCourse.Order.Domain.Entities;
+
+public class OrderItem : BaseEntity<int>
+{
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; } = null!;
+    public decimal UnitPrice { get; set; }
+    public Guid OrderId { get; set; }
+    public Order Order { get; set; } = null!;
+
+    public void SetItem(Guid productId, string productName, decimal unitPrice)
+    {
+        if (string.IsNullOrEmpty(ProductName))
+            throw new ArgumentNullException(nameof(ProductName), "ProductName cannot be empty.");
+
+        if (UnitPrice <= 0)
+            throw new ArgumentNullException(nameof(UnitPrice), "UnitPrice cannot be less than or equal to zero.");
+
+        this.ProductId = productId;
+        this.ProductName = productName;
+        this.UnitPrice = unitPrice;
+    }
+
+    public void UpdatePrice(decimal newPrice)
+    {
+        if (newPrice <= 0)
+            throw new ArgumentNullException("NewPrice cannot be less than or equal to zero.");
+
+        this.UnitPrice = newPrice;
+    }
+
+    public void ApplyDiscount(float discountPercentage)
+    {
+        if (discountPercentage < 0 || discountPercentage > 100)
+            throw new ArgumentNullException("Discount percentage must be between 0 and 100");
+
+        this.UnitPrice -= (this.UnitPrice * (decimal)discountPercentage / 100);
+    }
+
+    public bool IsSameItem(OrderItem orderItem)
+    {
+        return this.ProductId == orderItem.ProductId;
+    }
+}
